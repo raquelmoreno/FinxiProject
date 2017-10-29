bc3f3147f59ac48a056e0bfac6670d9edf1856f9
raquelmoreno <raquelcoite@hotmail.com>
2017-10-28 23:09:38 -0200

    page OcupaMesa initial

--- src/pages/ocupa-mesas/ocupa-mesas.ts
+++ src/pages/ocupa-mesas/ocupa-mesas.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the OcupaMesasPage page.
@@ -15,6 +16,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OcupaMesasPage {
 
+  public selected:string;
+
+  items = [
+    "Mesa 1",
+    "Mesa 2",
+    "Mesa 3",
+    "Mesa 4"
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -22,4 +32,16 @@ export class OcupaMesasPage {
     console.log('ionViewDidLoad OcupaMesasPage');
   }
 
+  goToPage() {
+    this.navCtrl.setRoot(HomePage);
+ }
+
+ itemSelected(item:string){
+   this.selected = item.trim();
+ }
+
+ ocupar(){
+   console.log(this.selected+ " ocupada");
+   this.goToPage();
+ }
 }
